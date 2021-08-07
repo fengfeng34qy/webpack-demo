12f2cfe6a3ecbc92f18a9efb7004b440b492a4ba
孙锋锋 <sunfengfeng@agree.com.cn>
2021-08-07 14:09:10 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
     mode: 'development', // development | production
     entry: path.join(__dirname, './src/index.js'),
     output: {
+        // 表示输出文件的存放路径
         path: path.join(__dirname, './dist'),
+        // 表示输出文件的名称
         filename: 'bundle.js'
     }
 }
